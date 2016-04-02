6f33f2578b16d35f1f90d5739465249072dfc96b
ycloo <yc.lu@berkeley.edu>
2016-04-02 01:06:16 +0000

    done ets page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,5 +4,6 @@ class MainController < ApplicationController
     # redirect_to '/faculty/dashboard' if faculty_login?
     # redirect_to '/admin/dashboard' if admin_login?
   end
-
+  def ets
+  end
 end
